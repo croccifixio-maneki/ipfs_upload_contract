aa10e39b41b62296b9a41c6197e0a9c521b17f51
delena <delenamaneki@gmail.com>
2021-08-31 11:21:12 +0700

    Add file to ipfs node

---
+++ README.md
@@ -0,0 +1,26 @@
+## Get Started
+
+### Prerequisites
+
+install go version `>=1.15`
+
+### Installation
+
+Install ipfs_upload command
+
+```
+cd ipfs_upload
+go install
+```
+
+### Run Development Node
+
+Install IPFS
+
+You’ll need to download it for your platform from this link: https://ipfs.io/docs/install/.
+
+To start a development IPFS node:
+
+```
+ipfs daemon
+```
---
+++ ipfs_upload/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	shell "github.com/ipfs/go-ipfs-api"
+)
+
+const (
+	LOCAL_NODE = "http://localhost:8080/ipfs"
+	USAGE      = "ipfs_upload file_path"
+	URL        = "localhost:5001"
+)
+
+func main() {
+	sh := shell.NewShell(URL)
+	if len(os.Args) < 2 {
+		fmt.Println(USAGE)
+		os.Exit(1)
+	}
+
+	path := os.Args[1]
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Err open file: %s: %+v \n", path, err)
+		os.Exit(1)
+	}
+
+	cid, err := sh.Add(f)
+	if err != nil {
+		fmt.Printf("Err add to IPFS with URL:%s err:%+v \n", URL, err)
+		os.Exit(1)
+	}
+	fmt.Printf("CID added: %s \n", cid)
+	fmt.Printf("Access file at: %s \n", fmt.Sprintf("%s/%s", LOCAL_NODE, cid))
+}

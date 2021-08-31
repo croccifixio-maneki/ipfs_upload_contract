89fe2c8c591d3fe2c345cfc26c76d78d5122e4da
delena <delenamaneki@gmail.com>
2021-08-31 23:10:27 +0700

    Update test

--- contract_reg/test/cid-test.js
+++ contract_reg/test/cid-test.js
@@ -12,16 +12,30 @@ describe("Test CIDs", function () {
     await cid_Contract.deployed();
   
     await cid_Contract.setOwner(CID, owner.address)
-    expect((await cid_Contract.isOwner("Qma83cBXemC9a6acWZ9wwtNSJ1WzxXyGwTstiGbrooY8ih", owner.address))).to.equal(true);
-    expect((await cid_Contract.isOwner("Qma83cBXemC9a6acWZ9wwtNSJ1WzxXyGwTstiGbrooY8ih", not_owner.address))).to.equal(false);
+  });
 
-
-    await cid_Contract.setOwner(CID, not_owner.address)
-    expect((await cid_Contract.isOwner("Qma83cBXemC9a6acWZ9wwtNSJ1WzxXyGwTstiGbrooY8ih", not_owner.address))).to.equal(true);
-
+  it("Should NOT allow to set the CID ", async function () {
+    const CID_FACTORY = await hre.ethers.getContractFactory("CIDContract");
+    const cid_Contract = await CID_FACTORY.deploy();
+    const [owner, not_owner] = await hre.ethers.getSigners();
+    await cid_Contract.deployed();
+  
+    await cid_Contract.setOwner(CID, owner.address)
     await expectRevert(
-        cid_Contract.setOwner(CID, owner.address),
+        cid_Contract.connect(not_owner).setOwner(CID, not_owner.address),
         'caller is not the owner',
       );
+  });
+
+  it("Changes owner", async function () {
+    const CID_FACTORY = await hre.ethers.getContractFactory("CIDContract");
+    const cid_Contract = await CID_FACTORY.deploy();
+    const [owner, owner2] = await hre.ethers.getSigners();
+    await cid_Contract.deployed();
+  
+    await cid_Contract.setOwner(CID, owner.address)
+    await cid_Contract.setOwner(CID, owner2.address)
+
+    expect((await cid_Contract.isOwner("Qma83cBXemC9a6acWZ9wwtNSJ1WzxXyGwTstiGbrooY8ih", owner2.address))).to.equal(true);
   });
 });
